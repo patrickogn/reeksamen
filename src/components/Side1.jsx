809ba0d971bcb2e4579254b7e1dde07993493b56
Patrick Nielsen <cph-pn157@cphbusiness.dk>
2023-01-21 19:06:01 +0100

    startkode klar

---
+++ src/components/Side1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Side1 = () => {
+  return (
+    <div style={{padding: 30}} className='column middle'>
+      dette er side 1
+      dawdawdawd
+      dawdawdawddawdawd
+    </div>
+  )
+}
+
+export default Side1
---
+++ src/settings.js
@@ -0,0 +1,4 @@
+//const URL = "http://localhost:8080/24timers";
+const URL = "https://patrickognielsen.dk/tomcat/24timers";
+
+export default URL;
